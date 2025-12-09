96939247a09cf2633b559d47b03945f7d3f14cae
Tao.z <lizouzt@gmail.com>
2025-12-09 22:38:35 +0800

    feat: add electron.process

--- src/preload.ts
+++ src/preload.ts
@@ -1,6 +1,9 @@
 import { contextBridge, ipcRenderer } from 'electron'
 
 contextBridge.exposeInMainWorld('electron', {
+  process: {
+    platform: process.platform
+  },
   ipcRenderer: {
     invoke: (channel: string, ...args: any[]) => ipcRenderer.invoke(channel, ...args),
     send: (channel: string, ...args: any[]) => ipcRenderer.send(channel, ...args),
--- src/renderer.tsx
+++ src/renderer.tsx
@@ -38,6 +38,7 @@ renderApp()
 // MacOS 上红绿灯默认在标题栏左侧，因此需要将窗口标题文字让开以避免遮挡，这里设为居中。
 // 需要注意这里使用了 electron-vite 框架，如果你没有使用该框架，可在 Node 后端将 process.platform 传递给前端进行判断。
 // 'darwin' 代表 MacOS
+console.log('window.electron?.process?.platform', window.electron?.process?.platform)
 if (window.electron?.process?.platform === 'darwin') {
   document.querySelector('body')?.classList.add('darwin')
 }
